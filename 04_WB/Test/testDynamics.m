1e4ef749c165e6eaf5b3d66dbb1e291d0d19e28e
John_Nganga <69316705+jnganga1@users.noreply.github.com>
2023-12-13 11:07:54 -0500

    Original
    Before any changes --> Particularly before adding flywheel

--- 04_WB/Test/testDynamics.m
+++ 04_WB/Test/testDynamics.m
@@ -1,12 +1,13 @@
 addpath(genpath('../'));
 addpath(genpath("../../../spatial_v2_extended"));
-addpath('/home/heli/Sources/casadi-linux-matlabR2014b-v3.5.5', '-end');
+% addpath('/home/heli/Sources/casadi-linux-matlabR2014b-v3.5.5', '-end');
+addpath('C:\Program Files (x86)\casadi-windows-matlabR2016a-v3.5.3','-end');
 
 %% get function hanldes
 params = getMiniCheetahParams();
 robot = buildTreeModelWithRotor(params);
 % robot = buildTreeModel(params);
-dynFuncs = DynamicsSupport(robot);
+dynFuncs = WBDynamicsSupport(robot);
 
 
 %% Test dynamics
